0b05f934dbd84ebd1144c22f8288d92796a5692e
raj-aryan25 <74666685+raj-aryan25@users.noreply.github.com>
2022-11-29 09:02:37 +0530

    Update main_window.py

--- main_window.py
+++ main_window.py
@@ -1,9 +1,5 @@
 from tkinter import *
 import mysql.connector
-import loginsignup as ls
-
-exe = mysql.connector.connect(user='root', host='localhost',password='tiger25dec',database='rajlakshya')
-cur = exe.cursor()
 
 main = Tk()
 main.geometry("1300x700")
@@ -12,12 +8,20 @@ main.configure(bg='#ebf5fb')
 main.resizable(False,False)
 heading = Label(main,text="NUTRITION MANAGEMENT",bg="#D35400",fg="white", font = ("Arial",30))
 heading.pack(fill=BOTH,padx=20,pady=10)
+welcomeframe = Frame(main, bg='#85C1E9', width = 500,height = 550)
+welcomeframe.place(x=750,y=90)
+sideframe = Frame(main,bg= "olive", width = 600,height = 565)
+sideframe.place(x=50,y=80)
+projectname = Label(sideframe, width = 14,text="School\nNutritionist",fg="#E74C3C",font=("Tahoma",50))
+projectname.place(x=22,y=30)
+welcome = '''Welcome to School Nutritionist
+Your own School Nutrition Management System'''
+Label(sideframe,text=welcome,font=).place(x=22,y=250)
 
-mainframe = Frame(main, bg = '#d2fc82', width = 1000, height = 580)
-mainframe.pack(fill = BOTH, padx=20,pady=20)
 
 
 
 
-
+#mainframe = Frame(main, bg = '#d2fc82', width = 1000, height = 580)
+#mainframe.pack(fill = BOTH, padx=20,pady=20)
 main.mainloop()
