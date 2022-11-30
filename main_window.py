1a373c0cb3f511b87c322cc6b7dcf22c7ceaa554
raj-aryan25 <74666685+raj-aryan25@users.noreply.github.com>
2022-11-30 13:16:19 +0530

    Add files via upload

--- main_window.py
+++ main_window.py
@@ -4,6 +4,7 @@ import mysql.connector
 def nextscr():
         welcomeframe.destroy()
         sideframe.destroy()
+        
 main = Tk()
 main.geometry("1300x700")
 main.title("School Nutritionist")
@@ -13,6 +14,10 @@ style1= ("Helvetica",18)
 heading = Label(main,text="NUTRITION MANAGEMENT",bg="#D35400",fg="white", font = ("Arial",30))
 heading.pack(fill=BOTH,padx=20,pady=10)
 welcomeframe = Frame(main, bg='#85C1E9', width = 500,height = 550)
+loginframe = Frame(main, bg='#85C1E9', width = 500,height = 550)
+signupframe = Frame(main, bg='#85C1E9', width = 500,height = 550)
+signupframe.place(x=700,y=90)
+loginframe.place(x=700,y=90)
 welcomeframe.place(x=700,y=90)
 sideframe = Frame(main,bg= "#ebf5fb", width = 600,height = 565)
 sideframe.place(x=50,y=80)
@@ -22,20 +27,21 @@ img_lbl.place(x=50,y=10)
 projectname = Label(sideframe,bg='#A3E4D7', width = 14,text="SCHOOL\nNUTRITIONIST",fg="#E74C3C",font=("Tahoma",50))
 projectname.place(x=22,y=400)
 
-#btn1 = Button(welcomeframe,text="delete",command=deleteh)
+#btn1 = Button(welcomeframe,text="delete",command=nextscr)
 #btn1.place(x=20,y=200)
 
 welcome = '''Welcome to School Nutritionist\n
 Your own School Nutrition Management System'''
 Label(welcomeframe,text=welcome,font=("Tahoma",16)).place(x=22,y=30)
 Label(welcomeframe,text='Login to Continue', font = style1,width=30).place(x=30,y=180)
-Button(welcomeframe,text="Login",font=style1).place(x=80,y=280)
+Button(welcomeframe,text="Login",font=style1,width = 16,command=lambda:loginframe.tkraise()).place(x=130,y=240)
+Label(welcomeframe, text='or',bg='#85C1E9',font=style1).place(x=240,y=300)
 
-Label(welcomeframe,text='Login to Continue', font = style1,width=30).place(x=30,y=180)
-Button(welcomeframe,text="Login",font=style1).place(x=80,y=280)
+Label(welcomeframe,text='New to the App?\nCreate New Account', font = style1,width=30).place(x=32,y=340)
+Button(welcomeframe,text="Signup",font=style1,width=16,command=lambda:signupframe.tkraise()).place(x=130,y=430)
 
 
-
+welcomeframe.tkraise()
 
 
 
